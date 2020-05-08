a12f4fe30c67e31ae9454bef28b1ecfb8c2a9a79
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-05-08 11:14:43 +0300

    added nuget and models

---
+++ MVC/MVC/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CustomIdentityApp.Models
+{
+    public class User : IdentityUser
+    {
+        public int Name { get; set; }
+        public int Surname { get; set; }
+        public int Email { get; set; }
+    }
+}
--- MVC/MVC/Startup.cs
+++ MVC/MVC/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using CustomIdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace MVC
 {
@@ -23,6 +26,12 @@ namespace MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+
             services.AddControllersWithViews();
         }
 
@@ -44,6 +53,7 @@ namespace MVC
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
