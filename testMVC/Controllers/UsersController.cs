cb6c96f289325ed3d2681766c584b8343cef6750
Serg-Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-05-13 11:54:38 +0300

    added users list

---
+++ testMVC/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using CustomIdentityApp.Models;
+using CustomIdentityApp.ViewModels;
+
+namespace testMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_userManager.Users.ToList());
+        }
+    }
+}
