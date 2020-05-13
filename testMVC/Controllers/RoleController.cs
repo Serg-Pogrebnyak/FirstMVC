1b9db335de87ef3dc1ad879aa5d3d3cc20b16341
Serg-Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-05-13 13:09:58 +0300

    added roles

---
+++ testMVC/Controllers/RoleController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using testMVC.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace testMVC.Controllers
+{
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        [HttpGet]
+        public IActionResult Index() => View(_roleManager.Roles.ToList());
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(RoleViewModel model) 
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(model.Name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Role");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ testMVC/ViewModels/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testMVC.ViewModels
+{
+    public class RoleViewModel
+    {
+        [Required]
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+    }
+}
