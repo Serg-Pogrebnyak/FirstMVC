d4c1f7a68d75efda0c300706eded7ba1fffd4072
Serg-Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-05-14 18:26:09 +0300

    added categories view and controller

---
+++ testMVC/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using testMVC.DataBase;
+using testMVC.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace testMVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            using (DBContext db = new DBContext())
+            {
+                Categories[] categories = db.Categoies.ToArray();
+                return View(categories);
+            }
+        }
+    }
+}
