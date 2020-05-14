ded83e08b697e525c42e8ef4e0e36daadb8c20d0
Serg-Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-05-14 18:09:34 +0300

    added Entity Framework and categories

---
+++ testMVC/DataBase/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using testMVC.Models;
+
+namespace testMVC.DataBase
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Categories> Categoies { get; set; }
+        public DBContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
