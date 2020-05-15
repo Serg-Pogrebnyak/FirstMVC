902c50da452eb09aff2198fb71164e4dbbd565a2
Serg-Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-05-15 12:44:37 +0300

    added view and model for create new product

--- testMVC/Controllers/ProductsController.cs
+++ testMVC/Controllers/ProductsController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using testMVC.DataBase;
+using testMVC.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,6 +20,29 @@ namespace testMVC.Controllers
             {
                 return View(db.Products.ToArray());
             }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            using (DBContext db = new DBContext())
+            { 
+                SelectList categories = new SelectList(db.Categories, "Name");
+                ViewBag.Categories = db.Categories.ToArray();
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductViewModel newProduct)
+        {
+            //using (DBContext db = new DBContext())
+            //{
+            //    SelectList categories = new SelectList(db.Categories, "Name");
+            //    ViewBag.Categories = db.Categories.ToArray();
+            //    return View();
+            //}
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ testMVC/ViewModels/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using testMVC.Models;
+
+namespace testMVC.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Required]
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+        
+        [Required]
+        [Display(Name = "Цена")]
+        public int Price { get; set; }
+
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "В какую категорию добавить:")]
+        public String Category { get; set; }
+    }
+}
