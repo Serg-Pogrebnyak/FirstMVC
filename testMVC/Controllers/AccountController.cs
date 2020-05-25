f5392fc2db4712e50112a0da63f982203fd90032
Serg-Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-05-25 16:51:36 +0300

    added same basket logic when user add product in basket and after that - register -> migrate basket from cookie to DB

--- testMVC/Controllers/AccountController.cs
+++ testMVC/Controllers/AccountController.cs
@@ -38,23 +38,24 @@ namespace CustomIdentityApp.Controllers
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-
+                    //confirm email service
                     // генерация токена для пользователя
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Action(
-                        "ConfirmEmail",
-                        "Account",
-                        new { userId = user.Id, code = code },
-                        protocol: HttpContext.Request.Scheme);
-                    EmailService emailService = new EmailService();
-                    await emailService.SendEmailAsync(model.Email, "Confirm your account",
-                        $"Подтвердите регистрацию, перейдя по ссылке: <a href='{callbackUrl}'>link</a>");
-
-                    return Content("Для завершения регистрации проверьте электронную почту и перейдите по ссылке, указанной в письме");
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //var callbackUrl = Url.Action(
+                    //    "ConfirmEmail",
+                    //    "Account",
+                    //    new { userId = user.Id, code = code },
+                    //    protocol: HttpContext.Request.Scheme);
+                    //EmailService emailService = new EmailService();
+                    //await emailService.SendEmailAsync(model.Email, "Confirm your account",
+                    //    $"Подтвердите регистрацию, перейдя по ссылке: <a href='{callbackUrl}'>link</a>");
+                    //return Content("Для завершения регистрации проверьте электронную почту и перейдите по ссылке, указанной в письме");
                     
+                    migrateBasketFromCookieToDB(user.Id);
+
                     // установка куки
-                    //await _signInManager.SignInAsync(user, false);
-                    //return RedirectToAction("Index", "Home");
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
                 }
                 else
                 {
@@ -107,18 +108,16 @@ namespace CustomIdentityApp.Controllers
                     await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
                 if (result.Succeeded)
                 {
-                    if (HttpContext.Session.Keys.Contains("basket"))
-                    {
-                        _orderService.migrateBasketFromCookie(user.Id, HttpContext.Session.GetString("basket"));
-                        HttpContext.Session.Remove("basket");
-                    }
+                    migrateBasketFromCookieToDB(user.Id);
 
-                    if (!user.EmailConfirmed)
-                    {
-                        await _signInManager.SignOutAsync();
-                        ModelState.AddModelError("", "Email not confirmed");
-                        return View(model);
-                    }
+                    //check confirmed email
+                    //if (!user.EmailConfirmed)
+                    //{
+                    //    await _signInManager.SignOutAsync();
+                    //    ModelState.AddModelError("", "Email not confirmed");
+                    //    return View(model);
+                    //}
+
                     // проверяем, принадлежит ли URL приложению
                     if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
                     {
@@ -143,6 +142,15 @@ namespace CustomIdentityApp.Controllers
             // удаляем аутентификационные куки
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
+        }
+
+        private void migrateBasketFromCookieToDB(string userId)
+        {
+            if (HttpContext.Session.Keys.Contains("basket"))
+            {
+                _orderService.migrateBasketFromCookie(userId, HttpContext.Session.GetString("basket"));
+                HttpContext.Session.Remove("basket");
+            }
         }
     }
 }
